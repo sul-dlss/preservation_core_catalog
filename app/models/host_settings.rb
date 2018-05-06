0d19a3aaf6a47e2065e0e272cd38f01bbb7c3d34
Johnathan Martin <john.martin@stanford.edu>
2018-05-06 16:39:31 -0700

    WIP archive endpoint configuration and seeding

--- app/models/host_settings.rb
+++ app/models/host_settings.rb
@@ -1,8 +1,17 @@
 # Lets us consolidate shared_configs for almost identical machines
 class HostSettings
+  def self.archive_endpoints
+    Settings.archive_endpoint_map[archive_endpoint_lookup_name] || {}
+  end
+
   # @return [Config::Options, Hash] key/value pairs of name: file_path
   def self.storage_roots
     Settings.storage_root_map[storage_root_lookup_name] || {}
+  end
+
+  # @return [String] hostname depending on environment
+  def self.archive_endpoint_lookup_name
+    Rails.env.production? ? parsed_hostname : 'default'
   end
 
   # @return [String] hostname depending on environment
