f11fa5058adb92f051fe2942bc4903fb54475ca0
Tony Zanella <tony.zanella@gmail.com>
2019-03-18 13:10:40 -0700

    Deploy to the rest of sds

--- config/deploy/sds.rb
+++ config/deploy/sds.rb
@@ -1,7 +1,7 @@
 server 'preservation-catalog-sds-01.stanford.edu', user: 'pres', roles: %w[app db web]
-# server 'preservation-catalog-sds-02.stanford.edu', user: 'pres', roles: %w[app resque queue_populator]
-# server 'preservation-catalog-sds-03.stanford.edu', user: 'pres', roles: %w[app resque]
-# server 'preservation-catalog-sds-04.stanford.edu', user: 'pres', roles: %w[app resque cache_cleaner]
+server 'preservation-catalog-sds-02.stanford.edu', user: 'pres', roles: %w[app resque queue_populator]
+server 'preservation-catalog-sds-03.stanford.edu', user: 'pres', roles: %w[app resque]
+server 'preservation-catalog-sds-04.stanford.edu', user: 'pres', roles: %w[app resque cache_cleaner]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
