4ec950cd83e954564434725138f791f1ad9042cf
Tony Zanella <tony.zanella@gmail.com>
2019-10-22 16:19:21 -0700

    Add deployment to sul-backup-3

---
+++ config/deploy/zfs.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+server 'sul-backup-3.stanford.edu', user: 'pres', roles: %w[app db web resque]
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
+set :bundle_without, 'deploy test'
+set :deploy_to, '/opt/app/pres/preservation_catalog'
+append :linked_files, "config/newrelic.yml", "config/resque.yml"
