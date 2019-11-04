1dcfa5b6dcc1a4aec0fcdb07846e2c0114e1753d
Tony Zanella <tony.zanella@gmail.com>
2019-11-04 15:06:09 -0800

    Add test buckets to zfs deployment

--- config/deploy/zfs.rb
+++ config/deploy/zfs.rb
@@ -8,3 +8,7 @@ set :bundle_without, 'deploy test'
 set :deploy_to, '/opt/app/pres/preservation_catalog'
 append :linked_files, "config/newrelic.yml", "config/resque.yml"
 set :rvm_custom_path, '/usr/local/rvm'
+
+set :east_bucket_name, 'sul-sdr-aws-us-east-1-test'
+set :west_bucket_name, 'sul-sdr-aws-us-west-2-test'
+set :south_bucket_name, 'sul-sdr-ibm-us-south-1-test'
